202f63473796a9540a6fe17d1429b076c15eff57
Saksham Kashyap <kashyapsaksham012@gmail.com>
2025-07-08 00:57:23 +0530

    Updated files with latest changes

--- src/components/Auth/TaskList/TaskList.jsx
+++ src/components/Auth/TaskList/TaskList.jsx
@@ -4,38 +4,65 @@ const TaskList = () => {
   return (
     <div id="tasklist" className='h-[55%] flex  items-center justify-start gap-5  flex-nowrap w-full py-5 overflow-x-auto mt-10'>
 
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-amber-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
 
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
       </div>
 
-    <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-green-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
 
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
       </div>
 
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-pink-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
 
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
+      </div>
+ 
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-cyan-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
+
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
       </div>
 
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-blue-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
 
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
       </div>
 
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
+      <div className='h-full flex-shrink-0 p-5 full w-[300px] bg-red-300  rounded-xl'>
+              <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 feb 2024</h4>
+              </div>
 
+              <h2 className='mt-5 text-3xl font-semibold'>Make sure you regularly do DSA</h2>
+              <p className='mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci, maxime aliquam libero voluptate tempora.</p>
       </div>
-
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
-
-      </div>
-
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
-
-      </div>
-
-      <div className='h-full flex-shrink-0 full w-[300px] bg-amber-300  rounded-xl'>
-
-      </div>
-      
 
     </div>
   )
